51d0b610dc2545963e43b57223fe75a332b2b5d5
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-07-22 13:06:47 +0200

    sm7325: Build AdvancedDisplay from hardware/samsung
    Change-Id: Iab195edee0fcfedcd7373568ac49a811afcd9563

--- common.mk
+++ common.mk
@@ -142,7 +142,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    AdvancedDisplay
 
 # DRM
 PRODUCT_PACKAGES += \
