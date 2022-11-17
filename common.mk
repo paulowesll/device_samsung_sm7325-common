fe48bdf7f724b7add5d09fa990dac49b33341f7c
weswolf <31709861+paulowesll@users.noreply.github.com>
2022-11-17 20:12:35 -0300

    sm7325: build android.hardware.camera.provider@2.6.vendor

--- common.mk
+++ common.mk
@@ -111,6 +111,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-legacy \
     android.hardware.camera.provider@2.5-legacy \
+    android.hardware.camera.provider@2.6.vendor \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     camera.device@3.3-impl \
