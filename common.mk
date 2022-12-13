cc7fbece8e8a0e13e3e7307a61b452ada250f1e9
weswolf <31709861+paulowesll@users.noreply.github.com>
2022-12-12 22:33:45 -0300

    sm7325: fingerprint: fix typo

--- common.mk
+++ common.mk
@@ -179,7 +179,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service-samsung.sm7325
+    android.hardware.biometrics.fingerprint@2.3-service.samsung.sm7325
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
