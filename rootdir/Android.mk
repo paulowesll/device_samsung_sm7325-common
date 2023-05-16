b36a180bdd8f4bf50851170e858ec0e54749b4d1
Wesley <31709861+paulowesll@users.noreply.github.com>
2023-05-16 18:31:56 -0300

    sm7325: remove duplicated wifi_sec.rc

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -141,14 +141,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := wifi_sec.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/wifi_sec.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := wifi_qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
